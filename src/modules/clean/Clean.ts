1404d8e796601079202c953d128ab657285c4273
Starcea <stardev.uwu@gmail.com>
2024-04-05 21:04:25 +0900

    feat: insert timestamp in cleaned channel

--- src/modules/clean/Clean.ts
+++ src/modules/clean/Clean.ts
@@ -17,6 +17,10 @@ const cleanChannel = async (db: PrismaClient, chn: Channel | null) => {
 
   const newChannel = await chn.clone()
 
+  await newChannel.setName(
+    chn.name.slice(0, -8) + new Date().toISOString().slice(0, 10)
+  )
+
   await db.cleanChannel.updateMany({
     where: {
       id: chn.id,
